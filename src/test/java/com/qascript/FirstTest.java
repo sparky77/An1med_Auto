dc3cd1d9973c2714dc88ee0c9e114a25ebde0cb6
Marcus Smith <marcus.sparky77@gmail.com>
2023-04-02 01:45:46 +0100

    MS maven running

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,5 +1,6 @@
 package com.qascript;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -12,14 +13,15 @@ public class FirstTest {
     @Test
     public void OpenBrowser()  {
         WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         options.addArguments("disable-gpu");
         driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
+        driver.get("https://www.animeddirect.co.uk/");
         System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Pet Foods | Pet Prescription Medication | Pet Accessories"));
     }
 
 

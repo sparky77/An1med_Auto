79560d66f05323a1ade810d77a2fc3ae48302050
Marcus Smith <marcus.sparky77@gmail.com>
2023-04-02 01:59:59 +0100

    MS push event test

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -12,6 +12,7 @@ public class FirstTest {
 
     @Test
     public void OpenBrowser()  {
+
         WebDriver driver;
         WebDriverManager.chromedriver().setup();
         //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
@@ -22,6 +23,7 @@ public class FirstTest {
         driver.get("https://www.animeddirect.co.uk/");
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Pet Foods | Pet Prescription Medication | Pet Accessories"));
+        driver.quit();
     }
 
 

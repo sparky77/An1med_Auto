fc06f4e8c687007df6a67b791c9a897ffff963dc
Marcus Smith <marcus.sparky77@gmail.com>
2023-06-29 01:18:39 +0100

    MS updating POM for allure

--- src/test/java/co/uk/animed/StepDef.java
+++ src/test/java/co/uk/animed/StepDef.java
@@ -17,7 +17,7 @@ public class StepDef {
     @Given("Customer performs search for pet stock")
     public void customerSearchesForStock() {
         firstTest.customerActions("search","doggie treats");
-        System.out.println("Testing for ci p");
+        System.out.println("Testing for ci p3");
         //Customer customer = new Customer();
         //customer.runSearch("doggie treats");
     }

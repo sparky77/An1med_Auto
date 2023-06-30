dca4a1180b94d10c1367062dcb8d79ac51740edc
Marcus Smith <marcus.sparky77@gmail.com>
2023-06-30 15:50:59 +0100

    MS pushing for sonar

--- src/test/java/Helper/PageHelper.java
+++ src/test/java/Helper/PageHelper.java
@@ -67,7 +67,6 @@ public class PageHelper {
                 System.out.println("-------------------------");
                 System.out.println("-------------------------");
                 System.out.println("-------------------------");
-                System.out.println("-------------------------");
                 System.out.println("Invalid customer action: " + action);
                 break;
         }
